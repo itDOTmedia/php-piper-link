6228470456b6f9fb52dac795bc019c811966e969
Peter Grundmann <mail@peter-grundmann.de>
2024-11-07 12:10:49 +0100

    base model custom data

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -1,36 +1,74 @@
 <?php
+
 namespace Idm\PiperLink\Models;
 
 use ArrayAccess;
-use Idm\PiperLink\Types\Operation;
 
 abstract class BaseModel implements ArrayAccess
 {
     #region customs
 
-    public $customs = [];
+    /**
+     *
+     * @var \Idm\PiperLink\Models\CustomData[]
+     */
+    public array $customs = [];
 
-    public function offsetSet($offset, $value): void {
+    public function offsetSet($offset, $value): void
+    {
         // BaseModel[] = $value;
         if (is_null($offset)) {
             $this->customs[] = $value;
-        } 
+        }
         // BaseModel[$key] = $value;
         else {
             $this->customs[$offset] = $value;
         }
     }
 
-    public function offsetExists($key): bool {
+    public function offsetExists($key): bool
+    {
         return isset($this->customs[$key]);
     }
 
-    public function offsetUnset($key): void {
+    public function offsetUnset($key): void
+    {
         unset($this->customs[$key]);
     }
 
-    public function offsetGet($key): mixed {
+    public function offsetGet($key): mixed
+    {
         return $this->customs[$key] ?? null;
+    }
+
+    /**
+     * Get custom data object by key and optional language.
+     *
+     * @param string $key The custom data key.
+     * @param string|null $lang The optional custom language selector.
+     * @return mixed Returns the custom data object if the key (and language) exists, null otherwise.
+     */
+    public function getCustom(string $key, ?string $lang = null): ?CustomData
+    {
+        foreach ($this->customs as $data) {
+            if ($data->key === $key && ($lang == null || $data->language == $lang)) {
+                return $data;
+            }
+        }
+        
+        return null;
+    }
+    
+    /**
+     * Get custom value by key and optional language.
+     *
+     * @param string $key The custom data key.
+     * @param string|null $lang The optional custom language selector.
+     * @return mixed Returns the custom data value if the key (and language) exists, null otherwise.
+     */
+    public function getCustomValue(string $key, ?string $lang = null): mixed
+    {
+        return $this->getCustom($key, $lang)?->value ?? null;
     }
 
     #endregion
---
+++ src/Models/CustomData.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Idm\PiperLink\Models;
+
+/**
+ * Custom data.
+ * Custom data used for non-standard model data.
+ */
+class CustomData
+{
+    public ?string $key = null;
+    public ?string $language = null;
+    public ?string $source = null;
+    public ?string $type = null;
+    public ?string $value = null;
+}
